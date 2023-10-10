a6670dbcb7a10e52d527b0d81ddfcb3134eb5c36
poocha006 <joppan624@gmail.com>
2023-10-10 10:14:53 +0400

    practice5.py

--- Chapter 1- Getting Started with Python/practice5.py
+++ Chapter 1- Getting Started with Python/practice5.py
@@ -1,1 +1,7 @@
 #write four strings in different variables and print the output as one string(string/concantenation)
+string1 = "luffy"
+string2 = "is"
+string3 = "the"
+string4 = "king"
+result = string1 + string2 + string3 + string4
+print (result)
---
+++ Chapter 1- Getting Started with Python/python6.py
@@ -0,0 +1,5 @@
+f_name = "Joseph"
+l_name = "John"
+name = f_name +l_name
+print (name,end="")
+print (" Welcome to code lab 1")
